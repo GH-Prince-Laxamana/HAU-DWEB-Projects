3017b3979cd8293b550749b5c42846798cd5e546
GH-Prince-Laxamana <prince.laxamana116@gmail.com>
2025-11-29 20:42:24 +0800

    Added HTML and PHP boilerplates, Added file structures

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Store</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+    
+</body>
+</html>
